59cfbd1024cc36a37a9d6dd8560395a912aa53fe
sani <khursani8@gmail.com>
2018-02-19 00:07:55 +0800

    add squeezenet

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp  = require('gulp')
 let uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
+const gzip = require('gulp-gzip');
 
 // create a default task and just log a message
 gulp.task('default', function() {
+    gulp.src('./resnet/output_keras/**/*')
+        .pipe(gzip())
+        .pipe(gulp.dest('./resnet'));
     return gulp.src('./js/**/*.js')
         .pipe(concat('bundle.js'))
         .pipe(uglify())
+        .pipe(gzip())
         .pipe(gulp.dest('./data/'));
 });
---
+++ squeeze_net/README.md
@@ -0,0 +1,5 @@
+# SqueezeNet
+
+Example of [SqueezeNet](https://arxiv.org/abs/1602.07360)[landola+, 2016] image classification.
+
+The model source code and pretrained model is from [https://github.com/wohlert/keras-squeezenet](https://github.com/wohlert/keras-squeezenet)
