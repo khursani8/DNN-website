3c94e68ecd12d300d2fe7bc73ce59c10fe47de52
sani <khursani8@gmail.com>
2018-02-19 13:19:50 +0800

    add progress bar

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ const gzip = require('gulp-gzip');
 
 // create a default task and just log a message
 gulp.task('default', function() {
-    gulp.src('./resnet/output_keras/**/*')
-        .pipe(gzip())
-        .pipe(gulp.dest('./resnet/output_keras'));
+    // gulp.src('./resnet/output_keras/**/*')
+    //     .pipe(gzip())
+    //     .pipe(gulp.dest('./resnet/output_keras'));
     return gulp.src('./js/**/*.js')
         .pipe(concat('bundle.js'))
         .pipe(uglify())
---
+++ resnet/progress.js
@@ -0,0 +1,21 @@
+var Progress = (function () {
+    function Progress (p) {
+	this.bar = document.querySelectorAll('#prog-bar > .progress-bar')[0];
+	this.p = p;
+	this.update();
+    };
+    Progress.prototype.update = function ()  {
+	this.bar.style.width = this.p + '%';
+    };
+    Progress.prototype.countup = function () {
+	if (this.p < 100) { this.p += 10; }
+	this.update();
+    };
+    Progress.prototype.countdown = function () {
+	if (0 < this.p) { this.p -= 10; }
+	this.update();
+    };
+    return Progress;
+}());
+
+var p = new Progress(0);
